5a5121885b9b7bc1035dbda209283bd7b8f2c0bb
ilektrym <ilektr19@gmail.com>
2023-05-21 19:07:55 +0300

    lab4-5

---
+++ lab4-5/app.py
@@ -0,0 +1,67 @@
+import requests
+from flask import Flask, render_template, request, redirect
+import psycopg2
+
+app = Flask(__name__)
+
+conn = psycopg2.connect(database="service_db",
+                        user="postgres",
+                        password="12DImA567",
+                        host="localhost",
+                        port="5432")
+
+cursor = conn.cursor()
+
+
+@app.route('/login/', methods=['GET'])
+def index():
+    return render_template('login.html')
+
+
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')
+            password = request.form.get('password')
+            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+            records = list(cursor.fetchall())
+            if records == [] and username != '' and password != '':
+                return render_template('login.html', tx='Не верно введён Username или Password!')
+
+            if username == '' or password == '':
+                return render_template('login.html', tx='Не введён Username или Password!')
+
+            if records != []:
+                return render_template('account.html', full_name=records[0][1],
+                                       login=records[0][2], password=records[0][3])
+        elif request.form.get("registration"):
+            return redirect("/registration/")
+
+    return render_template('login.html')
+
+
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        login = request.form.get('login')
+        password = request.form.get('password')
+        cursor.execute("SELECT * FROM service.users WHERE login=%s", (str(login),))
+        rec = list(cursor.fetchall())
+        if name != '' or login != '' or password != '':
+            if rec != [] and name != '' and login != '' and password != '':
+                return render_template('registration.html', tx='login уже занят!')
+            else:
+                cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);',
+                               (str(name), str(login), str(password)))
+                conn.commit()
+                return redirect('/login/')
+        else:
+            return render_template('registration.html', tx='Не введино имя, логин или пароль!')
+
+    return render_template('registration.html')
+
+
+if __name__ == '__main__':
+    app.run()
